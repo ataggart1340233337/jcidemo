ffd17586d1ab995c18182f8632933d44381301f2
adam <ataggart13@qub.ac.uk>
2021-10-19 11:30:58 +0100

    Added a variable

--- src/uk/ac/qub/eeecs/dave/Calculator.java
+++ src/uk/ac/qub/eeecs/dave/Calculator.java
@@ -3,7 +3,11 @@ package uk.ac.qub.eeecs.dave;
 public class Calculator {
 	public int add(int a, int b) {
 		return a+b+1;
+		
+		
 	}
+
+	int test = 10;
 	
 	public int sub(int a, int b) {
 		return a-b;
